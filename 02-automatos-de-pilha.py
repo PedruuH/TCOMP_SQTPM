07e52e428dc0af8f6f9c365bf5b0a994cfbfb8e4
PedruuH <pedro.inventador@hotmail.com>
2021-09-22 16:36:07 -0300

    adequando aos automatos de pilha

--- 02-automatos-de-pilha.py
+++ 02-automatos-de-pilha.py
@@ -3,19 +3,22 @@
 
 n = None                            # número de estados n
 simbolos_t = None                   # conjunto de símbolos terminais (Σ)
+cj_s_pilha = None                   # conjunto de símbolos de pilha (Γ)
 cj_F = None                         # conjunto de estados de aceitação (F)
 n_transicoes = None                 # número t de transições (δ) do autômato
 transicoes=[]                       # lista com as transicoes possiveis do automato
 n_cadeias = None                    # número c de cadeias de entrada que serão avaliadas.
 cadeias = []                        # lista que conterá as cadeias que serão avaliadas
 aceitacao = None                    # usada para avaliação da aceitacao das cadeias
-estado_inicial = 0
+nSimbolosTerminais = None           # quantidade de simbolos terminais
+SimbolosTerminais = None            # lista de transições
 
 
 def init():                                     # função primaria para captura 
-    global n, simbolos_t, cj_F, n_transicoes, qtd_simbolsF    # dos parâmetros iniciais do automato
+    global n, simbolos_t, cj_F, n_transicoes, cj_s_pilha    # dos parâmetros iniciais do automato
     n = int(input())
     simbolos_t = input().split(' ')
+    cj_s_pilha = input().split(' ')
     cj_F = input().split(' ')
     n_transicoes = int(input())
 
@@ -37,10 +40,13 @@ def get_cadeias():                             # capturar as cadeias que serão 
 
 
 
+
 if __name__ == "__main__":
     init()
     get_transicoes()
     n_cadeias = int(input())                    # obter o numero c de cadeias
     get_cadeias()
     aceitacao = list(map(int, cj_F[1:]))
+    nSimbolosTerminais = simbolos_t[0]
+    SimbolosTerminais = simbolos_t[1:]
     
